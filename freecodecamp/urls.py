0e60729b1d41389a03797ae5b900b10e8328ac84
MohamedGouaouri <im_gouaouri@esi.dz>
2021-09-01 15:44:59 +0100

    refactor urls.py

--- freecodecamp/urls.py
+++ freecodecamp/urls.py
@@ -19,8 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
-    path("blogs/", include("blog.urls")),
     path("chat/", include('chat.urls')),
 
 ]
