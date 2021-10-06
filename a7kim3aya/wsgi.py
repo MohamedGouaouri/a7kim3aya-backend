ceddd38e54136a4dddf961afb689d6dc8e07700e
MohamedGouaouri <im_gouaouri@esi.dz>
2021-10-06 13:02:22 +0100

    change project name to a7kim3aya remove secret

--- a7kim3aya/wsgi.py
+++ a7kim3aya/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for freecodecamp project.
+WSGI config for a7kim3aya project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freecodecamp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a7kim3aya.settings')
 
 application = get_wsgi_application()
--- products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path
-from .views import *
-
-urlpatterns = [
-    path("", home, name='home'),
-    path("list/", ProductAPIView.as_view()),
-    path("create/", CreateProductAPIView.as_view()),
-]
